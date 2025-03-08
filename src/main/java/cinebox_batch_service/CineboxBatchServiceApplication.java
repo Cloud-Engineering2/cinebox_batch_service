44a34c1c7a19712d2526c03de34033a4198dd48c
Bal1oon <ws32159@naver.com>
2025-03-08 23:01:24 +0900

    init: Initialize setting

---
+++ src/main/java/cinebox_batch_service/CineboxBatchServiceApplication.java
@@ -0,0 +1,13 @@
+package cinebox_batch_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineboxBatchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineboxBatchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cinebox_batch_service/CineboxBatchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cinebox_batch_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineboxBatchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
